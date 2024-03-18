7083cb139b1543621cce821016ecc37fe68493b8
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-03-18 17:53:35 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX  = 0;
+rightWristY = 0;
 
 function preLoad()
 {
@@ -14,9 +18,32 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY)
+    }
 }
