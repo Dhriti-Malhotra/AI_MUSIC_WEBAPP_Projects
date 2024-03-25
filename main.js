269455b623e06a8ea1d71d279a45de12a46f72b6
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-03-25 18:06:48 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,33 @@ function draw()
     fill('#7B68EE');
     stroke('#6A5ACD')
 
+    circle(rightWristX, rightWristY, 20)
+    if(rightWristY > 0 && rightWristY <= 100)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <=200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <=300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <=400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <=500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+
     if(scoreLeftWrist > 0.2)
     {
     circle(leftWristX, leftWristY, 15);
@@ -46,7 +73,7 @@ function draw()
     }
     if(song1 = 'true')
     {
-        rightWrist = "Aukaat.mp3".isPlaying();
+        rightWrist = "Aukaat.mp3".isPlaying(true);
     }
     else
     {
@@ -54,7 +81,7 @@ function draw()
     }
     if(song2 = 'true')
     {
-        rightWrist = "Jatt_Vailly.mp3".isPlaying();
+        rightWrist = "Jatt_Vailly.mp3".isPlaying(true);
     }
     else
     {
@@ -72,8 +99,9 @@ function gotPoses()
     if(results.length > 0)
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = "+ scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
